593e5c3c72bfb750bb46d9d6faff987b4888c91c
LEEJEONGJAE <st20916@naver.com>
2025-08-22 22:01:53 +0900

    design : modify gallery detail page

--- src/js/job/sub.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('.detail__btn__container')
-    , button = container.querySelector('button');
-
-button.addEventListener('click', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-
-  location.href = '../../html/job/job.html';
-});
