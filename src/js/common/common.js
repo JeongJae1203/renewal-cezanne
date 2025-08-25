37b7ababc21c8aba4b329267473bc73ca65f9273
st20916 <st20916@naver.com>
2025-08-25 16:11:59 +0900

    design : add sitemap in sub page

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -3,3 +3,28 @@ document.addEventListener('click', (e) => {
     e.preventDefault();
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const topBtn = document.querySelector('#topBtn')
+      , btnEl = topBtn.querySelector('button');
+
+  /**
+   * Window Event Scroll
+   */
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+      topBtn.classList.add('visible');
+    } else {
+      topBtn.classList.remove('visible');
+    }
+  });
+
+  /**
+   * Top Button Click Event
+   */
+  btnEl.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
