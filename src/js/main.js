d9239f4f026ecc5798836e7e6d524d8001d287d6
hollolo123 <hollolo123@gmail.com>
2025-08-25 17:20:44 +0900

    design :: main portfolio and etc

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // a링크 기본 설정 값
+  document.addEventListener('click', (e) => {
+    if(e.target.matches('a[href="#"]')) {
+      e.preventDefault();
+    }
+  });
+
+  // swiper :: mainSlide
   new Swiper('.main_visual', {
     loop:true,
     autoplay:true,
@@ -7,4 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
       el:'.swiper-pagination',
     }
   });
+
+  // swiper :: portfolio 
+  new Swiper('.slideArea', {
+    loop : true,
+    effect : "coverflow",
+    grabCursor : true,
+    centeredSlides : true,
+    slidesPerView : "auto",
+    initialSlide : 1,
+    coverflowEffect : {
+      rotate : 30,
+      stretch : 0,
+      depth : 100,
+      modifier : 1,
+      slideShadows : true,
+    },
+    pagination : {
+      el : ".swiper-pagination",
+    }
+  });
+
 });
