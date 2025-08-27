c944f9d7c65300cbd4ad4864b3f1e1c9ef7cb17a
st20916 <st20916@naver.com>
2025-08-27 18:23:27 +0900

    design : modify portfolio

--- src/js/portfolio/portfolio.js
+++ src/js/portfolio/portfolio.js
@@ -1,3 +1,5 @@
+import { createTab, totalTop } from '../function';
+
 // a태그에 링크가 없는 경우
 document.addEventListener('click', (e) => {
   if(e.target.matches('a[href="#"]')) {
@@ -5,3 +7,19 @@ document.addEventListener('click', (e) => {
   }
 });
 
+
+const licenseEl = document.querySelector('.common__tab')
+      , tabEls = licenseEl.querySelectorAll('.portfolio__tab')
+      , tabWrap = licenseEl.querySelector('.tab__contents')
+      , tabContentEls = licenseEl.querySelectorAll('.tab__content')
+      , clazz = 'active';
+
+createTab(tabEls, tabWrap, tabContentEls, clazz);
+
+
+tabContentEls.forEach(tabContentEl => {
+  const topEl = tabContentEl.querySelector('.common__top')
+      , liEls = tabContentEl.querySelectorAll('.portfolio__list > li');
+  
+  totalTop(topEl, liEls);
+})
